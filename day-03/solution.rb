9954929a620d6b94f42e371649a3be822f118390
Damien Gonot <damien.gonot@gmail.com>
2024-12-03 14:37:27 -0500

    day 3 part 2 in Ruby

--- day-03/solution.rb
+++ day-03/solution.rb
@@ -5,3 +5,16 @@ instructions = memory.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
 result = instructions.map { |a, b| a.to_i * b.to_i }.sum
 
 puts "Part 1: #{result}"
+
+instructions2 = memory.scan(/(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))/)
+
+filtered = instructions2.inject([]) do |result, element|
+  @ignore = true if element == ["don't()", nil, nil]
+  result << element unless @ignore
+  @ignore = false if element == ["do()", nil, nil]
+  result
+end
+
+result2 = filtered.map { |_, a, b| a.to_i * b.to_i }.sum
+
+puts "Part 2: #{result2}"
