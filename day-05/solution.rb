f6d9854d05f6a27dc69e62fac91eff544f5036ba
Damien Gonot <damien.gonot@gmail.com>
2024-12-05 09:41:50 -0500

    day 5 part 1 in Ruby

---
+++ day-05/solution.rb
@@ -0,0 +1,24 @@
+manual = File.readlines("example.txt", chomp: true).reject { |e| e.empty? }
+
+raw_rules, raw_pages = manual.partition { |x| x.include?('|') }
+
+rules = raw_rules.map { |e| e.split('|').map { |i| i.to_i } }
+
+pages = raw_pages.map { |e| e.split(',').map { |i| i.to_i } }
+
+def valid?(rules, update)
+  rules.each do |p1, p2|
+    next if !update.include?(p1) || !update.include?(p2)
+    return false if update.index(p1) > update.index(p2)
+  end
+
+  true
+end
+
+valid_pages = pages.select { |p| valid?(rules, p) }
+
+middle_pages = valid_pages.map { |pages| pages[pages.length / 2] }
+
+result = middle_pages.sum
+
+puts "Part 1: #{result}"
