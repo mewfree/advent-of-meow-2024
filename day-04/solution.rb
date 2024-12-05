61ff6b024ca201455e3d5ebf5a8ce05642bfc802
Damien Gonot <damien.gonot@gmail.com>
2024-12-04 23:27:17 -0500

    day 4 part 2 in Ruby

--- day-04/solution.rb
+++ day-04/solution.rb
@@ -39,3 +39,32 @@ end
 result = count_word(grid)
 
 puts "Part 1: #{result}"
+
+def count_x(grid)
+  rows = grid.length
+  cols = grid[0].length
+
+  def valid?(grid, x, y)
+    return false if x < 1 || x >= grid.length - 1
+    return false if y < 1 || y >= grid[0].length - 1
+
+    tlbr = (grid[x-1][y-1] == 'M' && grid[x+1][y+1] == 'S') || (grid[x-1][y-1] == 'S' && grid[x+1][y+1] == 'M')
+    trbl = (grid[x-1][y+1] == 'M' && grid[x+1][y-1] == 'S') || (grid[x-1][y+1] == 'S' && grid[x+1][y-1] == 'M')
+    tlbr && trbl
+  end
+
+  count = 0
+  (0...rows).each do |x|
+    (0...cols).each do |y|
+      if grid[x][y] == 'A' && valid?(grid, x, y)
+        count += 1
+      end
+    end
+  end
+
+  count
+end
+
+result2 = count_x(grid)
+
+puts "Part 2: #{result2}"
