4f594a78d4a1355b166cd819ef3f05bfbca71fd9
Damien Gonot <damien.gonot@gmail.com>
2024-12-05 21:04:18 -0500

    day 5 part 2 in Ruby

--- day-05/solution.rb
+++ day-05/solution.rb
@@ -22,3 +22,20 @@ middle_pages = valid_pages.map { |pages| pages[pages.length / 2] }
 result = middle_pages.sum
 
 puts "Part 1: #{result}"
+
+invalid_pages = pages.reject { |p| valid?(rules, p) }
+
+def sort_page(rules, page)
+  page.sort do |a, b|
+    rule = rules.find { |x, y| [x, y].sort == [a, b].sort }
+    rule.index(a) <=> rule.index(b)
+  end
+end
+
+reordered_pages = invalid_pages.map { |page| sort_page(rules, page) }
+
+middle_pages2 = reordered_pages.map { |pages| pages[pages.length / 2] }
+
+result2 = middle_pages2.sum
+
+puts "Part 2: #{result2}"
