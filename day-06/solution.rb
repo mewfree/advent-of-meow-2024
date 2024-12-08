3006e3457cda5357a4949487a8f671b8ec707e8b
Damien Gonot <damien.gonot@gmail.com>
2024-12-08 10:08:17 -0500

    day 6 part 2 in Ruby

--- day-06/solution.rb
+++ day-06/solution.rb
@@ -1,4 +1,5 @@
-map = File.readlines("example.txt", chomp: true).map(&:chars)
+base_map = File.readlines("example.txt", chomp: true).map(&:chars).freeze
+map = base_map.map(&:dup)
 
 y = map.find_index { |row| row.include?('^') }
 x = map[y].find_index('^')
@@ -33,3 +34,55 @@ end
 result = map.flatten.count('X')
 
 puts "Part 1: #{result}"
+
+previous_coordinates = []
+
+map.each_with_index do |row, row_index|
+  row.each_with_index do |element, col_index|
+    previous_coordinates << [row_index, col_index] if element == 'X'
+  end
+end
+
+options = []
+
+previous_coordinates.each do |by, bx|
+  tracker = Set.new
+
+  # Reset map and directions
+  map = base_map.map(&:dup)
+  directions = direction_deltas.keys.cycle
+  direction = directions.next
+
+  y = map.find_index { |row| row.include?('^') }
+  x = map[y].find_index('^')
+
+  next if [x, y] == [bx, by]
+
+  # Mark bx, by as "blocked"
+  next if map[by].nil?
+  map[by][bx] = '#'
+
+  loop do
+    dy, dx = direction_deltas[direction]
+    ny, nx = y + dy, x + dx
+
+    break if !ny.between?(0, map.length - 1)
+    break if !nx.between?(0, map.first.length - 1)
+
+    if map[ny][nx] == '#'
+      direction = directions.next
+    else
+      y, x = ny, nx
+      id = [x, y, direction]
+      if tracker.include?(id)
+        options << [bx, by]
+        break
+      end
+      tracker << id
+    end
+  end
+end
+
+result2 = options.count
+
+puts "Part 2: #{result2}"
