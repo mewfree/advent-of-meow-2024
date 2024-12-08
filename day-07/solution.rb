bc4ec3dc38a6a328a5661b903b34895f6199cb72
Damien Gonot <damien.gonot@gmail.com>
2024-12-08 16:00:24 -0500

    day 7 part 2 in Ruby

--- day-07/solution.rb
+++ day-07/solution.rb
@@ -4,20 +4,32 @@ calibrations = File.readlines("example.txt", chomp: true).inject({}) do |hash, c
   hash
 end
 
-operators = [:+, :*].freeze
+operators = [
+  ->(a, b) { a + b },
+  ->(a, b) { a * b },
+]
 
-results = calibrations.map do |test_value, numbers|
-  operation_sequences = operators.repeated_permutation(numbers.size - 1).to_a
-  result = operation_sequences.map do |operation_sequence|
-    result = numbers.first
-    numbers[1..].each_with_index do |number, index|
-      result = result.send(operation_sequence[index], number)
-    end
-    result if result == test_value
+def calculate_results(calibrations, operators)
+  calibrations.map do |test_value, numbers|
+    operation_sequences = operators.repeated_permutation(numbers.size - 1).to_a
+    operation_sequences.map do |operation_sequence|
+      result = numbers.first
+      numbers[1..].each_with_index do |number, index|
+        result = operation_sequence[index].call(result, number)
+      end
+      result if result == test_value
+    end.compact.uniq
   end
-  result.compact.uniq
 end
 
+results = calculate_results(calibrations, operators)
 result = results.flatten.sum
 
 puts "Part 1: #{result}"
+
+operators << ->(a, b) { (a.to_s + b.to_s).to_i }
+
+results2 = calculate_results(calibrations, operators)
+result2 = results2.flatten.sum
+
+puts "Part 2: #{result2}"
